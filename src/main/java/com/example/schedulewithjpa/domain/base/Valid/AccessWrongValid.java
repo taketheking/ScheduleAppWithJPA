a06a5486c5b5133e57191f50e01f62425916a39d
taketheking <so4524@gmail.com>
2024-11-15 06:11:31 +0900

    fix : AccessMisMatchId true -> false
    잘못된 비교 결과값을 !로 변경

--- src/main/java/com/example/schedulewithjpa/domain/base/Valid/AccessWrongValid.java
+++ src/main/java/com/example/schedulewithjpa/domain/base/Valid/AccessWrongValid.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 public class AccessWrongValid {
 
     public void AccessMisMatchId(Long numberOne, Long numberTwo) {
-        if(Objects.equals(numberOne, numberTwo)){
+        if(!Objects.equals(numberOne, numberTwo)){
             throw new NotMatchUserException("다른 유저의 정보를 잘못 접근하고 있습니다.");
         }
     }
